cce65ad595896609a655dfe46db2875c3b2ea132
Antushev.Yuriy <9217519273@mail.ru>
2022-03-10 19:47:47 +0300

    Домашнее задание (Преобразование типов)

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootsrap</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+            <div class="Top">
+                <div class="row align-items-center">
+                    <div class="col-2">
+                        <img src="img/Logo.svg" alt="Logo">
+                    </div>    
+                    <div class="col-8">
+                        <div class="Menu">
+                            <a target="_blank" href="https://www.Home.ru//">Home <img src="img/fi-rr-angle-small-down 1.svg" alt=">"></a>
+                            <a target="_blank" href="https://www.Service.ru//">Service <img src="img/fi-rr-angle-small-down 1.svg" alt=">"></a>
+                            <a target="_blank" href="https://www.Works.ru//">Works <img src="img/fi-rr-angle-small-down 1.svg" alt=">"></a>
+                            <a target="_blank" href="https://www.News.ru//">News <img src="img/fi-rr-angle-small-down 1.svg" alt=">"></a>
+                            <a target="_blank" href="https://www.Contact.ru//">Contact <img src="img/fi-rr-angle-small-down 1.svg" alt=">"></a>
+                        </div>    
+                    </div>
+                    <div class="col-2">
+                        <div class="MenuLogin">
+                            <a target="_blank" href="https://www.LogIn.ru//">Log In</a>
+                            <a target="_blank" href="https://www.SignUp.ru//">Sign Up</a>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-5">
+                <div class="Content">
+                    <h5>Bootstrap 5 theme</h5>
+                    <h3>crafted by <span>ThemeWagon</span></h3>
+                    <p>ThemeWagon offers an wide array of category-oriented Free and Premium  Bootstrap HTML Templates and Themes. </p>
+                    <button>Check Demo</button>
+            
+                </div>
+            </div>    
+            <div class="col-7">
+                <div class="Picture">
+                    <img src="img/Illusration.svg" alt="Illusration">
+                </div>
+            </div>    
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="Footer">
+            <div class="row align-items-center">
+                <div class="col-2">
+                    <img src="img/Group17.svg" alt="Group 17">
+                </div>    
+                <div class="col-2">
+                    <img src="img/Vector.svg" alt="IllusVectorration">
+                </div>    
+                <div class="col-3">
+                    <img src="img/Group18.svg" alt="Group 18">
+                </div>    
+                <div class="col-3">
+                    <img src="img/Group27.svg" alt="Group 27">
+                </div>    
+                <div class="col-2">
+                    <img src="img/final1.svg" alt="final 1">
+                </div>    
+            </div>
+        </div>
+    </div>
+    <footer>
+	<?php 
+	  echo "Year = " . date('Y'); 
+	?>
+    </footer>
+</body>
+</html>
